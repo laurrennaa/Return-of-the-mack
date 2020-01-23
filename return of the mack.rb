190ecebfa6622f95a77d9157e8dcdf2b7220fa32
Lauren Larios <54549719+laurrennaa@users.noreply.github.com>
2020-01-23 11:48:48 -0800

    Add files via upload

---
+++ return of the mack.rb
@@ -0,0 +1,119 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 95
+play :a4
+sleep 0.5
+play :c5
+sleep 0.5
+
+live_loop :background do
+  play :g3
+  play :a2
+  sleep 2
+  play :f3
+  play :d3
+  sleep 2
+end
+
+2.times do
+  play :d5
+  sleep 0.5
+  play :d5
+  sleep 0.5
+  play :d5
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :c5
+  sleep 1
+  play :a4
+  sleep 0.5
+  play :c5
+  sleep 0.5
+end
+
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :a4
+sleep 0.5
+play :c5
+sleep 1
+play :d5
+sleep 0.5
+play :e5
+sleep 0.5
+
+play :e5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :e5
+sleep 1
+play :a4
+sleep 0.5
+play :c5
+sleep 0.5
+
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :a4
+sleep 0.5
+play :c5
+sleep 1
+play :a4
+sleep 0.5
+play :c5
+sleep 0.5
+
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :a4
+sleep 0.5
+play :c5
+sleep 1
+play :a4
+sleep 0.5
+play :c5
+sleep 0.5
+
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :a4
+sleep 0.5
+play :c5
+sleep 1.5
+play :c5
+sleep 0.5
+
+play :b4
+sleep 0.5
+play :b4
+sleep 0.5
+play :a4
+sleep 0.5
+play :g4
+sleep 0.5
+play :b4
+sleep 1.5
+play :c5
+sleep 0.5
+
